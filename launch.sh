746a2fe6abb7f4618feb0e872dc85a2a4793ac86
Cainã Costa <me@cfcosta.com>
2021-12-22 23:36:25 -0300

    Use only dracula, rice some things

--- launch.sh
+++ launch.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# change the folder and reload to change the theme
-$(dirname $0)/themes/onedark/launch.sh
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar main -c $(dirname $0)/config.ini &
